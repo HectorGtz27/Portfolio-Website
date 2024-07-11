e848fc8a35f173fa8550263b215cd5b46c7f5576
HectorGtz27 <alan.gtz51@gmail.com>
2024-07-10 23:45:04 -0700

    chore: Refactor Contact component layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,36 +6,41 @@ import { getImageUrl } from "../../utils";
 function Contact() {
   return (
     <footer id="contact" className={styles.container}>
-      <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+      <div className={styles.innerContainer}>
+        <div className={styles.text}>
+          <h2>Contact</h2>
+          <p>Feel free to reach out!</p>
+        </div>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+            <a href="mailto:alan.gtz51@gmail.com" target="_blank">
+              alan.gtz51@gmail.com
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/linkedinIcon.png")}
+              alt="LinkedIn icon"
+            />
+            <a
+              href="https://www.linkedin.com/in/hector-guti%C3%A9rrez-941a2521a/"
+              target="_blank"
+            >
+              linkedin/hectorgutiérrez
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/githubIcon.png")}
+              alt="Github icon"
+            />
+            <a href="https://github.com/HectorGtz27" target="_blank">
+              github.com/HectorGtz27
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:alan.gtz51@gmail.com" target="_blank">
-            alan.gtz51@gmail.com
-          </a>
-        </li>
-        <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a
-            href="https://www.linkedin.com/in/hector-guti%C3%A9rrez-941a2521a/"
-            target="_blank"
-          >
-            linkedin/hectorgutiérrez
-          </a>
-        </li>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/HectorGtz27" target="_blank">
-            github.com/HectorGtz27
-          </a>
-        </li>
-      </ul>
     </footer>
   );
 }
