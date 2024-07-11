6d5986806e0f4e9ec523f167becd7997656f956b
HectorGtz27 <alan.gtz51@gmail.com>
2024-07-10 22:27:15 -0700

    Styling the project section cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Projects.module.css";
+import { FiArrowUpRight } from "react-icons/fi";
 
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
@@ -10,78 +11,117 @@ function Projects() {
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
       <div className={styles.gridContainer}>
-        <Link to="/projects-info" state={{ message: "Web Development" }}>
+        <Link
+          className={styles.link}
+          to="/projects-info"
+          state={{ message: "Web Development" }}
+        >
           <div className={styles.gridItem}>
-            <p>1</p>
-            <div>
-              <div>
-                <p>Web Development</p>
-                <p>Info</p>
-              </div>
-              <div>+</div>
+            <div className={styles.topCardContainer}>
+              <p className={styles.number}>1</p>
             </div>
-          </div>
-        </Link>
-        <Link to="/projects-info" state={{ message: "Machine Learning" }}>
-          <div className={styles.gridItem}>
-            <p>2</p>
-            <div>
-              <div>
-                <p>AI</p>
-                <p>Info</p>
+
+            <div className={styles.bottomCard}>
+              <div className={styles.sectionDescription}>
+                <p className={styles.sectionTitle}>Web Development</p>
+                <p className={styles.sectioSubtitle}>Info</p>
               </div>
-              <div>+</div>
-            </div>
-          </div>
-        </Link>
-        <Link to="/projects-info" state={{ message: "IoT" }}>
-          <div className={styles.gridItem}>
-            <p>3</p>
-            <div>
-              <div>
-                <p>IoT</p>
-                <p>Info</p>
-              </div>
-              <div>+</div>
+              <FiArrowUpRight size={62} />
             </div>
           </div>
         </Link>
         <Link
+          className={styles.link}
+          to="/projects-info"
+          state={{ message: "AI" }}
+        >
+          <div className={styles.gridItem}>
+            <div className={styles.topCardContainer}>
+              <p className={styles.number}>2</p>
+            </div>
+
+            <div className={styles.bottomCard}>
+              <div className={styles.sectionDescription}>
+                <p className={styles.sectionTitle}>AI</p>
+                <p className={styles.sectioSubtitle}>Info</p>
+              </div>
+              <FiArrowUpRight size={62} />
+            </div>
+          </div>
+        </Link>
+        <Link
+          className={styles.link}
+          to="/projects-info"
+          state={{ message: "IoT" }}
+        >
+          <div className={styles.gridItem}>
+            <div className={styles.topCardContainer}>
+              <p className={styles.number}>3</p>
+            </div>
+
+            <div className={styles.bottomCard}>
+              <div className={styles.sectionDescription}>
+                <p className={styles.sectionTitle}>IoT</p>
+                <p className={styles.sectioSubtitle}>Info</p>
+              </div>
+              <FiArrowUpRight size={62} />
+            </div>
+          </div>
+        </Link>
+        <Link
+          className={styles.link}
           to="/projects-info"
           state={{ message: "Object Oriented Programming" }}
         >
           <div className={styles.gridItem}>
-            <p>4</p>
-            <div>
-              <div>
-                <p>Object Oriented Programming</p>
-                <p>Info</p>
+            <div className={styles.topCardContainer}>
+              <p className={styles.number}>4</p>
+            </div>
+
+            <div className={styles.bottomCard}>
+              <div className={styles.sectionDescription}>
+                <p className={styles.sectionTitle}>Object Oriented</p>
+                <p className={styles.sectioSubtitle}>Info</p>
               </div>
-              <div>+</div>
+              <FiArrowUpRight size={62} />
             </div>
           </div>
         </Link>
-        <Link to="/projects-info" state={{ message: "Game Development" }}>
+        <Link
+          className={styles.link}
+          to="/projects-info"
+          state={{ message: "Game Development" }}
+        >
           <div className={styles.gridItem}>
-            <p>5</p>
-            <div>
-              <div>
-                <p>Game Development</p>
-                <p>Info</p>
+            <div className={styles.topCardContainer}>
+              <p className={styles.number}>5</p>
+            </div>
+
+            <div className={styles.bottomCard}>
+              <div className={styles.sectionDescription}>
+                <p className={styles.sectionTitle}>Game Development</p>
+                <p className={styles.sectioSubtitle}>Info</p>
               </div>
-              <div>+</div>
+              <FiArrowUpRight size={62} />
             </div>
           </div>
         </Link>
-        <Link to="/projects-info" state={{ message: "Other" }}>
+        <Link
+          className={styles.link}
+          to="/projects-info"
+          state={{ message: "Other" }}
+        >
           <div className={styles.gridItem}>
-            <p>6</p>
-            <div>
-              <div>
-                <p>Other</p>
-                <p>Info</p>
+            <div className={styles.topCardContainer}>
+              <p className={styles.number}>6</p>
+            </div>
+
+            <div className={styles.bottomCard}>
+              <div className={styles.sectionDescription}>
+                <p className={styles.sectionTitle}>Other</p>
+                <p className={styles.sectioSubtitle}>Info</p>
               </div>
-              <div>+</div>
+              <FiArrowUpRight size={62} />
             </div>
           </div>
         </Link>
