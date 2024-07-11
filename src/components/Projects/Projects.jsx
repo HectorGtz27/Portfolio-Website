8085a1dc6be3c8116bc11aa9776c395dab046f48
HectorGtz27 <alan.gtz51@gmail.com>
2024-07-10 21:52:43 -0700

    Implementing grid in the project sections cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,47 +9,89 @@ function Projects() {
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
-      <Link to="/projects-info" state={{ message: "Web Development" }}>
-        <div>
-          <p>1</p>
-          <div>
+      <div className={styles.gridContainer}>
+        <Link to="/projects-info" state={{ message: "Web Development" }}>
+          <div className={styles.gridItem}>
+            <p>1</p>
             <div>
-              <p>Web Development</p>
-              <p>Info</p>
+              <div>
+                <p>Web Development</p>
+                <p>Info</p>
+              </div>
+              <div>+</div>
             </div>
-            <div>+</div>
           </div>
-        </div>
-      </Link>
-      <Link to="/projects-info" state={{ message: "Machine Learning" }}>
-        <div>
-          <p>2</p>
-          <div>
+        </Link>
+        <Link to="/projects-info" state={{ message: "Machine Learning" }}>
+          <div className={styles.gridItem}>
+            <p>2</p>
             <div>
-              <p>AI</p>
-              <p>Info</p>
+              <div>
+                <p>AI</p>
+                <p>Info</p>
+              </div>
+              <div>+</div>
             </div>
-            <div>+</div>
           </div>
-        </div>
-      </Link>
-      <Link to="/projects-info" state={{ message: "IoT" }}>
-        <div>
-          <p>3</p>
-          <div>
+        </Link>
+        <Link to="/projects-info" state={{ message: "IoT" }}>
+          <div className={styles.gridItem}>
+            <p>3</p>
             <div>
-              <p>IoT</p>
-              <p>Info</p>
+              <div>
+                <p>IoT</p>
+                <p>Info</p>
+              </div>
+              <div>+</div>
             </div>
-            <div>+</div>
           </div>
-        </div>
-      </Link>
-      <div className={styles.projects}>
+        </Link>
+        <Link
+          to="/projects-info"
+          state={{ message: "Object Oriented Programming" }}
+        >
+          <div className={styles.gridItem}>
+            <p>4</p>
+            <div>
+              <div>
+                <p>Object Oriented Programming</p>
+                <p>Info</p>
+              </div>
+              <div>+</div>
+            </div>
+          </div>
+        </Link>
+        <Link to="/projects-info" state={{ message: "Game Development" }}>
+          <div className={styles.gridItem}>
+            <p>5</p>
+            <div>
+              <div>
+                <p>Game Development</p>
+                <p>Info</p>
+              </div>
+              <div>+</div>
+            </div>
+          </div>
+        </Link>
+        <Link to="/projects-info" state={{ message: "Other" }}>
+          <div className={styles.gridItem}>
+            <p>6</p>
+            <div>
+              <div>
+                <p>Other</p>
+                <p>Info</p>
+              </div>
+              <div>+</div>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      {/* <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
         })}
-      </div>
+      </div> */}
     </section>
   );
 }
