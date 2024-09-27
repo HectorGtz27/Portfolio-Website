1b150acd04f3416cc58ddb151b8f942fb915a25e
HectorGtz27 <alan.gtz51@gmail.com>
2024-09-26 21:09:14 -0700

    Refactor certifications.json and Hero.jsx components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,8 +8,9 @@ function Hero() {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Hector Gutierrez</h1>
         <p className={styles.description}>
-          I'm a passionate Computer Science student at Tecnológico de Monterrey,
-          Campus Sonora Norte. Reach out if you'd like to learn more!
+          I'm a passionate Computer Science student and Incoming Research
+          Assistant at Tecnológico de Monterrey, Campus Sonora Norte. Reach out
+          if you'd like to learn more!
         </p>
         <a href="mailto:alan.gtz51@gmail.com" className={styles.contactBtn}>
           Contact Me
